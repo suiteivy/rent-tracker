03007c19ff8fc21a2c41b7232e1db122bff59ec5
suiteivy <suiteivy036@gmail.com>
2025-08-28 12:50:34 +0300

    Delete src directory

--- src/supabaseClient.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-const supabaseUrl = 'https://ktemrlewjvdesvfbzlen.supabase.co'; 
-const supabaseanonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imt0ZW1ybGV3anZkZXN2ZmJ6bGVuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTM4NjQ3NTIsImV4cCI6MjA2OTQ0MDc1Mn0.1igWzAy3o-eBPT6cNWhHIXzPt-7XabefBwdleThaX1Y'; 
-
-export const supabase = createClient(supabaseUrl, supabaseanonKey);
